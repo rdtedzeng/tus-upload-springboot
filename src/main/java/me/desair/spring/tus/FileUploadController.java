2a0139ac2cdd8c7728ee7206aa7c820e1f99159f
jimmy0933 <34049916+jimmy0933@users.noreply.github.com>
2022-05-31 12:06:52 +0800

    jimmy modify
    jimmy modify

--- src/main/java/me/desair/spring/tus/FileUploadController.java
+++ src/main/java/me/desair/spring/tus/FileUploadController.java
@@ -1,11 +1,15 @@
 package me.desair.spring.tus;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,29 +22,72 @@ import me.desair.tus.server.upload.UploadInfo;
 @RequestMapping(value = "/api/upload")
 public class FileUploadController {
 
-    @Autowired
-    private TusFileUploadService tusFileUploadService;
+	@Autowired
+	private TusFileUploadService tusFileUploadService;
 
-    @RequestMapping(value = {"", "/**"}, method = {RequestMethod.POST, RequestMethod.PATCH, RequestMethod.HEAD,
-            RequestMethod.DELETE, RequestMethod.OPTIONS, RequestMethod.GET})
-    public void processUpload(final HttpServletRequest servletRequest, final HttpServletResponse servletResponse) throws IOException {
-        tusFileUploadService.process(servletRequest, servletResponse);
-        
-        String uploadURI = servletRequest.getRequestURI();
-        
-        UploadInfo uploadInfo = null;
-        
-        try {
+	@Value("${tus.server.data.directory}")
+	private String directory = "";
+
+	@RequestMapping(value = { "", "/**" }, method = { RequestMethod.POST, RequestMethod.PATCH, RequestMethod.HEAD,
+			RequestMethod.DELETE, RequestMethod.OPTIONS, RequestMethod.GET })
+	public void processUpload(final HttpServletRequest servletRequest, final HttpServletResponse servletResponse)
+			throws IOException {
+
+		System.out.println("process start:" + new Date());
+		tusFileUploadService.process(servletRequest, servletResponse);
+		System.out.println("process end:" + new Date());
+
+		String uploadURI = servletRequest.getRequestURI();
+
+		UploadInfo uploadInfo = null;
+
+		try {
 			uploadInfo = this.tusFileUploadService.getUploadInfo(uploadURI);
+			if (uploadInfo != null) {
+				System.out.println("uploadInfo.isUploadInProgress():" + uploadInfo.isUploadInProgress());
+			}else {
+				System.out.println("uploadInfo:null");
+			}
+
 		} catch (TusException e) {
 			e.printStackTrace();
 		}
-        
-        if (uploadInfo != null && !uploadInfo.isUploadInProgress()) {
-        	// 將檔案從暫存資料夾複製到指定路徑
-        	System.out.println("將檔案從暫存資料夾複製到指定路徑");
-        }
-        
+
+		if (uploadInfo != null && !uploadInfo.isUploadInProgress()) {
+
+			// 將檔案從暫存資料夾複製到指定路徑
+			File aDirectory = new File(directory);
+			String filenameString = uploadInfo.getFileName();
+			String serFilePath = aDirectory.getAbsolutePath() + File.separator + "uploads" + File.separator
+					+ filenameString + File.separator + "data";
+			File srcFile = new File(serFilePath);
+
+			File targetDirectory = new File(aDirectory.getAbsolutePath() + File.separator + "work" + File.separator);
+
+			if (!targetDirectory.exists()) {
+				targetDirectory.mkdir();
+
+			}
+
+			File target = new File(targetDirectory.getPath() + File.separator + filenameString);
+			if (target.exists()) {
+				target.delete();
+			}
+
+			System.out.println("copy start time:" + new Date());
+
+			Files.copy(srcFile.toPath(), target.toPath());
+
+			try {
+				// Thread.sleep(10000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("copy end time:" + new Date());
+			System.out.println("將檔案從暫存資料夾複製到指定路徑");
+		}
+
 //        try {
 //        	// 刪除上傳的暫存檔案
 //			this.tusFileUploadService.deleteUpload(uploadURI);
@@ -50,8 +97,13 @@ public class FileUploadController {
 //			System.out.println("Delete Upload "+ uploadInfo.getMetadata().get("fileName") +" Error: " + e);
 //			throw new RuntimeException(e);
 //		}
-        
-        System.out.println("=====上傳作業完成=====");
-    }
+
+		System.out.println("=====上傳作業完成=====" + new Date());
+		if (uploadInfo != null) {
+			System.out.println("uploadInfo.isUploadInProgress():" + uploadInfo.isUploadInProgress());
+		}else {
+			System.out.println("uploadInfo:null");
+		}
+	}
 
 }
--- src/main/resources/public/js/demo.js
+++ src/main/resources/public/js/demo.js
@@ -3,7 +3,7 @@
 
 "use strict";
 
-var upload          = null;
+
 var uploadIsRunning = false;
 var toggleBtn       = document.querySelector("#toggle-btn");
 var resumeCheckbox  = document.querySelector("#resume");
@@ -15,6 +15,9 @@ var uploadList      = document.querySelector("#upload-list");
 var chunkInput      = document.querySelector("#chunksize");
 var endpointInput   = document.querySelector("#endpoint");
 
+var input2           = document.querySelector("#toggle-input2");
+var input3           = document.querySelector("#toggle-input3");
+
 if (!tus.isSupported) {
   alertBox.classList.remove("hidden");
 }
@@ -23,31 +26,25 @@ if (!toggleBtn) {
   throw new Error("Toggle button not found on this page. Aborting upload-demo. ");
 }
 
-toggleBtn.addEventListener("click", function (e) {
-  e.preventDefault();
 
-  if (upload) {
-    if (uploadIsRunning) {
-      upload.abort();
-      toggleBtn.textContent = "resume upload";
-      uploadIsRunning = false;
-    } else {
-      upload.start();
-      toggleBtn.textContent = "pause upload";
-      uploadIsRunning = true;
-    }
-  } else {
-    if (input.files.length > 0) {
-      startUpload();
-    } else {
-      input.click();
-    }
-  }
+toggleBtn.addEventListener("click", function (e) {
+  starttus();
 });
 
-input.addEventListener("change", startUpload);
+
+function  starttus(){
+	startUpload();
+	//startUpload2();
+	//startUpload3();
+}
+
 
 function startUpload() {
+	
+var upload          = null;
+  if(console){
+	console.log("startUpload:"+new Date());
+	}
   var file = input.files[0];
   // Only continue if a file has actually been selected.
   // IE will trigger a change event even if we reset the input element
@@ -87,7 +84,15 @@ function startUpload() {
       progressBar.style.width = percentage + "%";
       console.log(bytesUploaded, bytesTotal, percentage + "%");
     },
+    onChunkComplete: function (chunkSize, bytesAccepted, bytesTotal) {
+	
+      console.log("onChunkComplete:"+new Date(),chunkSize, bytesAccepted, bytesTotal );
+
+ 
+     },
     onSuccess: function () {
+	console.log("onSuccess:"+new Date() );
+	
       var anchor = document.createElement("a");
       anchor.textContent = "Download " + upload.file.name + " (" + upload.file.size + " bytes)";
       anchor.href = upload.url;
@@ -106,6 +111,145 @@ function startUpload() {
 function reset() {
   input.value = "";
   toggleBtn.textContent = "start upload";
-  upload = null;
+  
   uploadIsRunning = false;
+}
+
+
+
+//jimmy add
+
+function startUpload2() {
+	var upload          = null;
+  if(console){
+	console.log("startUpload:"+new Date());
+	}
+  var file = input2.files[0];
+  // Only continue if a file has actually been selected.
+  // IE will trigger a change event even if we reset the input element
+  // using reset() and we do not want to blow up later.
+  if (!file) {
+    return;
+  }
+
+  var endpoint = endpointInput.value;
+  var chunkSize = parseInt(chunkInput.value, 10);
+  if (isNaN(chunkSize)) {
+    chunkSize = Infinity;
+  }
+
+  toggleBtn.textContent = "pause upload";
+
+  var options = {
+    endpoint: endpoint,
+    resume  : !resumeCheckbox.checked,
+    chunkSize: chunkSize,
+    retryDelays: [0, 1000, 3000, 5000],
+    onError : function (error) {
+      if (error.originalRequest) {
+        if (window.confirm("Failed because: " + error + "\nDo you want to retry?")) {
+          upload.start();
+          uploadIsRunning = true;
+          return;
+        }
+      } else {
+        window.alert("Failed because: " + error);
+      }
+
+      reset();
+    },
+    onProgress: function (bytesUploaded, bytesTotal) {
+      var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2);
+      progressBar.style.width = percentage + "%";
+      console.log(bytesUploaded, bytesTotal, percentage + "%");
+    },
+    onChunkComplete: function (chunkSize, bytesAccepted, bytesTotal) {
+	
+      console.log("onChunkComplete:"+new Date(),chunkSize, bytesAccepted, bytesTotal );
+
+ 
+     },
+    onSuccess: function () {
+	console.log("onSuccess:"+new Date() );
+      var anchor = document.createElement("a");
+      anchor.textContent = "Download " + upload.file.name + " (" + upload.file.size + " bytes)";
+      anchor.href = upload.url;
+      anchor.className = "btn btn-success";
+      uploadList.appendChild(anchor);
+
+      reset();
+    }
+  };
+
+  upload = new tus.Upload(file, options);
+  upload.start();
+  uploadIsRunning = true;
+}
+
+
+function startUpload3() {
+	var upload          = null;
+  if(console){
+	console.log("startUpload:"+new Date());
+	}
+  var file = input3.files[0];
+  // Only continue if a file has actually been selected.
+  // IE will trigger a change event even if we reset the input element
+  // using reset() and we do not want to blow up later.
+  if (!file) {
+    return;
+  }
+
+  var endpoint = endpointInput.value;
+  var chunkSize = parseInt(chunkInput.value, 10);
+  if (isNaN(chunkSize)) {
+    chunkSize = Infinity;
+  }
+
+  toggleBtn.textContent = "pause upload";
+
+  var options = {
+    endpoint: endpoint,
+    resume  : !resumeCheckbox.checked,
+    chunkSize: chunkSize,
+    retryDelays: [0, 1000, 3000, 5000],
+    onError : function (error) {
+      if (error.originalRequest) {
+        if (window.confirm("Failed because: " + error + "\nDo you want to retry?")) {
+          upload.start();
+          uploadIsRunning = true;
+          return;
+        }
+      } else {
+        window.alert("Failed because: " + error);
+      }
+
+      reset();
+    },
+    onProgress: function (bytesUploaded, bytesTotal) {
+      var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2);
+      progressBar.style.width = percentage + "%";
+      console.log(bytesUploaded, bytesTotal, percentage + "%");
+    },
+    onChunkComplete: function (chunkSize, bytesAccepted, bytesTotal) {
+	
+      console.log("onChunkComplete:"+new Date(),chunkSize, bytesAccepted, bytesTotal );
+
+ 
+     },
+    onSuccess: function () {
+	console.log("onSuccess:"+new Date() );
+      var anchor = document.createElement("a");
+      anchor.textContent = "Download " + upload.file.name + " (" + upload.file.size + " bytes)";
+      anchor.href = upload.url;
+      anchor.className = "btn btn-success";
+      uploadList.appendChild(anchor);
+
+      reset();
+    }
+  };
+
+  upload = new tus.Upload(file, options);
+  upload.start();
+  uploadIsRunning = true;
 }
